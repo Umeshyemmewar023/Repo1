fcef8560ca133883cfbdb17f1e94ebfdc4c9dbdb
Umeshyemmewar023 <umeshyemmewar0020@gmail.com>
2026-09-23 16:35:20 +0530

    Added merge conflict

--- Demo.c
+++ Demo.c
@@ -5,7 +5,7 @@ int main()
     printf("Hello World!");
     printf("Hello from Umesh Yemmewar");
     printf("I am learning github desktop");
-    printf("Checking the differnce b/w merge & rebase");
+    printf("Checking the merge conflict");
 
     printf("Mathematical Operations: ");
     
